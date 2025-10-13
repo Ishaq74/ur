cc406c1bda9aaf1650ae90dab9b542906c47b8bd
Ishaq74 <ishaq.achour@gmail.com>
2025-10-13 07:01:42 +0200

    Change site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   // Update with actual domain
-  site: 'https://salut-annecy.fr',
+  site: 'https://ur-ruddy.vercel.app',
 
   build: {
     inlineStylesheets: 'auto',
